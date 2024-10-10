f643797daef153799887f9bcf69d7305f6443bbc
Abril31 <brivaldi0416@gmail.com>
2024-10-10 00:59:22 -0400

    First commit

---
+++ back/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const num1 = 20;
+const num2 = 10;
+const sumar = (num1, num2) => {
+    return num1 + num2;
+};
+console.log(sumar(num1, num2));
---
+++ back/src/index.ts
@@ -0,0 +1,8 @@
+const num1: number = 20;
+const num2: number = 10;
+
+const sumar =  (num1: number, num2:number): number => {
+        return num1 + num2
+}
+
+console.log(sumar(num1, num2));
